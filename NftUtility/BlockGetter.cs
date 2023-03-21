cd9164e3c76ad54d097a30f79145738ad991874c
bin matsui <BinMatsui@hotmail.com>
2023-03-21 22:32:41 +0900

    SQL Write

--- NftUtility/BlockGetter.cs
+++ NftUtility/BlockGetter.cs
@@ -1,49 +1,92 @@
 ﻿using Nethereum.Hex.HexTypes;
 using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Web3;
-using System.Numerics;
-using System.Text.Json;
-using Web3Utility;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace NftUtility;
 
 public static class ContractaddressAccessor
 {
-    //public static async Task<Uri> ToImageUrlAsync(this string contractaddress, BigInteger tokenId, string infuraApiKey, HttpClient httpClient)
-    //{
-    //    var web3 = new Web3($"https://mainnet.infura.io/v3/{infuraApiKey}");
-    //    var request = await web3.Eth.GetBalance.SendRequestAsync(contractaddress);
-    //    var etherBalance = Web3.Convert.FromWei(request.Value);
+    public static async Task ToImageUrlAsync(string infuraApiKey, string connectionString)
+    {
+        var web3 = new Web3($"https://mainnet.infura.io/v3/{infuraApiKey}");
+        var blockNumber = await web3.Eth.Blocks.GetBlockNumber.SendRequestAsync();
+        for (int i = 0; i < blockNumber.Value; i++)
+        {
+            using var sql = new SqlConnection(connectionString);
+            sql.Open();
+            var blockWithTransactions = await web3.Eth.Blocks.GetBlockWithTransactionsByNumber.SendRequestAsync(new HexBigInteger(i));
+            sql.Insert($"select * from Block Where BlockHash = '{blockWithTransactions.BlockHash}'", blockWithTransactions.ToBlock());
+            foreach (var transaction in blockWithTransactions.Transactions)
+            {
+                sql.Insert($"select * from Block Where Transaction = '{transaction.BlockHash}'", transaction.ToTransaction());
+            }
+        }
+    }
 
-    //    //var transactionItems = new List<TransactionItem>();
-    //    var transactionCount = await web3.Eth.Transactions.GetTransactionCount.SendRequestAsync(contractaddress);
-    //    for (int i = 0; i < transactionCount.Value; i++)
-    //    {
-    //        var blockWithTransactions = await web3.Eth.Blocks.GetBlockWithTransactionsByNumber.SendRequestAsync(new HexBigInteger(i));
-    //        Transaction a = blockWithTransactions.Author;
-    //        //BlockWithTransactions
-    //    }
+    private static BlazorApp1.Shared.Transaction ToTransaction(this Transaction transaction)
+        => new ()
+        {
+            BlockHash = transaction.BlockHash,
+            BlockNumber = transaction.BlockNumber.ToString(),
+            From = transaction.From,
+            Gas = transaction.Gas.ToString(),
+            GasPrice = transaction.GasPrice.ToString(),
+            Input = transaction.Input,
+            MaxFeePerGas = transaction.MaxFeePerGas.ToString(),
+            MaxPriorityFeePerGas = transaction.MaxPriorityFeePerGas.ToString(),
+            Nonce = transaction.Nonce.ToString(),
+            R = transaction.R,
+            S = transaction.S,
+            To = transaction.To,
+            TransactionHash = transaction.TransactionHash,
+            TransactionIndex = transaction.TransactionIndex.ToString(),
+            Type = transaction.Type.ToString(),
+            V = transaction.V.ToString(),
+            Value = transaction.Value.ToString(),
+        };
 
-    //    //for (int i = 0; i < count.Value; i++)
-    //    //{
-    //    //    var bn = web3.Eth.Blocks.GetBlockWithTransactionsByNumber;
-    //    //    bn.SendRequestAsync(
 
-    //    //    var hash = web3.Eth.Transactions.GetTransactionByHash;
-    //    //    hash.SendRequestAsync()
+    private static BlazorApp1.Shared.Block ToBlock(this Block block)
+        => new()
+        {
+            Author = block.Author,
+            BaseFeePerGas = block.BaseFeePerGas.ToString(),
+            BlockHash = block.BlockHash.ToString(),
+            Difficulty = block.Difficulty.ToString(),
+            ExtraData = block.ExtraData.ToString(),
+            GasLimit = block.GasLimit.ToString(),
+            GasUsed = block.GasUsed.ToString(),
+            LogsBloom = block.LogsBloom.ToString(),
+            Miner = block.Miner.ToString(),
+            MixHash = block.MixHash.ToString(),
+            Nonce = block.Nonce.ToString(),
+            Number = block.Number.ToString(),
+            ParentHash = block.ParentHash.ToString(),
+            ReceiptsRoot = block.ReceiptsRoot.ToString(),
+            SealFields = block.SealFields.ToString(),
+            Sha3Uncles = block.Sha3Uncles.ToString(),
+            Size = block.Size.ToString(),
+            StateRoot = block.StateRoot.ToString(),
+            Timestamp = block.Timestamp.ToString(),
+            TotalDifficulty = block.TotalDifficulty.ToString(),
+            TransactionsRoot = block.TransactionsRoot.ToString(),
+            Uncles = block.Uncles.ToString(),
+        };
 
-    //    //    var receipt = web3.Eth.Transactions.GetTransactionByBlockNumberAndIndex;
-    //    //    var transaction = await receipt.SendRequestAsync(ethGetTransactionCount.DefaultBlock.BlockNumber, new HexBigInteger(i));
-    //    //    transactionItems.Add(new TransactionItem(
-    //    //        transaction.BlockHash,
-    //    //        transaction.BlockNumber,
-    //    //        transaction.From,
-    //    //        transaction.To,
-    //    //        transaction.Value,
-    //    //        transaction.Gas));
+    private static void Insert<T>(this SqlConnection sqlConnection, string query, T data)
+    {
+        var single = sqlConnection.Query<T>(query).SingleOrDefault();
+        if (single != null) return;
+        sqlConnection.Execute(data.GetInsertSql());
+    }
 
-    //    //}
-
-    //    //return new AddressItem(etherBalance, 0, transactionItems);
-    //}
+    private static string GetInsertSql<T>(this T data)
+    {
+        var properties = data.GetType().GetProperties();
+        var names = string.Join(",", properties.Select(x => x.Name));
+        var atNames = string.Join(",", properties.Select(x => "@" + x.Name));
+        return $@"insert into {data?.GetType()?.Name} ({names}) values ({atNames});";
+    }
 }
